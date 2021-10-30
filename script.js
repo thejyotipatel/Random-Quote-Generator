4d6dc8e353da0f4591aea35f04d279b4d86c35b1
Jyoti Patel <66724598+jyotip101@users.noreply.github.com>
2021-10-30 08:01:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const quote = document.querySelector(".quote");
-const author = document.querySelector(".author");
-const img = document.querySelector(".img");
+const author = document.querySelector(".author"); 
 const refresh = document.querySelector(".refresh");
 let ran = Math.floor(Math.random() * 1643);
 
@@ -11,8 +10,7 @@ fetch("https://type.fit/api/quotes").then(function(respones){
     }).then((data) =>{
         quote.textContent = data[ran].text; 
         author.textContent = "-"+ data[ran].author;
-});
-img.src = "https://picsum.photos/535/350/?blur=1.5&random=1";
+}); 
 
 // get quotes every time refresh
 refresh.addEventListener("click", function(){
@@ -25,7 +23,5 @@ refresh.addEventListener("click", function(){
         quote.textContent = data[ran].text; 
         author.textContent = "-"+ data[ran].author;
     });
-
-    // get random image every time refresh
-    img.src = "https://picsum.photos/535/350/?blur=1.5&random.webp";
+ 
 }); 
